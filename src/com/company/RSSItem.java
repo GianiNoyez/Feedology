001ef55a0c0b63016cbe84eba95e4be555a17fe9
Giani Noyez <giani.noyez@outlook.com>
2016-06-07 14:17:02 +0200

    basic RSSItem object
    Basic object which contains the title, link and description of an item
    found in a feed.

--- src/com/company/RSSItem.java
+++ src/com/company/RSSItem.java
@@ -3,15 +3,27 @@ package com.company;
 /**
  * Created by gianinoyez on 06/06/16.
  */
-public class RSSItem {
+public class RSSItem
+{
 
     private String title;
     private String link;
     private String description;
 
-    public RSSItem(String title, String link, String description) {
+    public RSSItem(String title, String link, String description)
+    {
         this.title = title;
         this.link = link;
         this.description = description;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "RSSItem{" +
+                "title='" + title + '\'' +
+                ", link='" + link + '\'' +
+                ", description='" + description + '\'' +
+                '}';
     }
 }
