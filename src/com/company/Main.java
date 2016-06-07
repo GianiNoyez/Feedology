8d451c9a0b057b12e0412062ffece614c3c64ef3
Giani Noyez <giani.noyez@outlook.com>
2016-06-07 15:10:21 +0200

    added extra rss feed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args)
     {
         RSSFeed rssFeed = new RSSFeed("http://feeds.ign.com/ign/all?format=xml");
+        RSSFeed rsssFeed = new RSSFeed("http://www.4gamers.be/rss/all-gamenews");
     }
 
 }
